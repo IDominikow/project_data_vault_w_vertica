35bf4a0d4474b81ae6acf1bc854b20d16b86adac
s-sushko <ssushko.aw@gmail.com>
2023-12-25 02:27:39 +0300

    Create load_s_auth_history.sql

---
+++ src/sql/load_s_auth_history.sql
@@ -0,0 +1,15 @@
+truncate table STV2023100611__DWH.s_auth_history;
+
+INSERT INTO STV2023100611__DWH.s_auth_history(hk_l_user_group_activity, user_id_from,event,event_dt,load_dt,load_src)
+select 
+	luga.hk_l_user_group_activity,
+	gl.user_id_from,
+	gl.event,
+	gl."datetime" as event_dt,
+	now() as load_dt,
+	's3' as load_src
+from STV2023100611__STAGING.group_log as gl
+left join STV2023100611__DWH.h_groups as hg on gl.group_id = hg.group_id
+left join STV2023100611__DWH.h_users as hu on gl.user_id = hu.user_id
+left join STV2023100611__DWH.l_user_group_activity as luga on hg.hk_group_id = luga.hk_group_id and hu.hk_user_id = luga.hk_user_id
+; 
