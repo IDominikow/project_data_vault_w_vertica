a61341b2fc2cf595f40701219b7be73c48f917dd
s-sushko <ssushko.aw@gmail.com>
2023-12-25 21:08:50 +0300

    Update create_group_log.sql

--- src/sql/create_group_log.sql
+++ src/sql/create_group_log.sql
@@ -6,7 +6,7 @@ create table if not exists STV2023100611__STAGING.group_log(
 	user_id integer,
 	user_id_from integer,
 	event varchar(20),
-	"datetime" timestamp(0)
+	event_dt timestamp(0)
 	)order by group_id, user_id
-	PARTITION BY "datetime"::date
-	GROUP BY calendar_hierarchy_day("datetime"::date, 3, 2);
+	PARTITION BY event_dt::date
+	GROUP BY calendar_hierarchy_day(event_dt::date, 3, 2);
